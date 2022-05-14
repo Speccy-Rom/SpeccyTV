67a1bb030207a30ed4021d1a86576350777bf2d2
speccy-rom <speccy.rom@gmail.com>
2022-05-14 22:21:12 +0300

    movies_async_api: create logger app

---
+++ services/movies_async_api/src/core/logger.py
@@ -0,0 +1,57 @@
+LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+LOG_DEFAULT_HANDLERS = ['console', ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': LOG_FORMAT
+        },
+        'default': {
+            '()': 'uvicorn.logging.DefaultFormatter',
+            'fmt': '%(levelprefix)s %(message)s',
+            'use_colors': None,
+        },
+        'access': {
+            '()': 'uvicorn.logging.AccessFormatter',
+            'fmt': "%(levelprefix)s %(client_addr)s - '%(request_line)s' %(status_code)s",
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+        'default': {
+            'formatter': 'default',
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
+        },
+        'access': {
+            'formatter': 'access',
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': LOG_DEFAULT_HANDLERS,
+            'level': 'INFO',
+        },
+        'uvicorn.error': {
+            'level': 'INFO',
+        },
+        'uvicorn.access': {
+            'handlers': ['access'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'formatter': 'verbose',
+        'handlers': LOG_DEFAULT_HANDLERS,
+    },
+}
