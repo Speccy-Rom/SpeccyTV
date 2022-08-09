d5cf7c9bf3aff08bb42223d8eae2c371307ba973
speccy-rom <speccy.rom@gmail.com>
2022-08-09 23:09:36 +0300

    movies_billing: fix refactoring

--- services/movies_billing/api/src/core/core.py
+++ services/movies_billing/api/src/core/core.py
@@ -3,17 +3,17 @@ from pydantic import BaseSettings, PostgresDsn
 
 class Settings(BaseSettings):
     postgres_dsn: PostgresDsn = 'postgresql+asyncpg://postgres:postgres@localhost:5432/subscriptions'
+
     project_name = 'Billing API'
     debug = True
     test = False
     stripe_secret_key: str = 'key'
-
     jwt_secret_key: str = '123'
     jwt_algorithm: str = 'HS256'
-
     host: str = 'http://localhost:8000'
     success_callback: str = host + '/v1/subscription/success?session_id={CHECKOUT_SESSION_ID}'
-    cancel_callback: str = host + '/v1/subscription/cancel'
+
+    cancel_callback: str = f'{host}/v1/subscription/cancel'
 
 
 settings = Settings()
