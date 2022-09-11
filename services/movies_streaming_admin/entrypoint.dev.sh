601c5a1b71d2bb831d321d3caf63dfdc6ecb6485
speccy-rom <speccy.rom@gmail.com>
2022-09-11 21:32:49 +0300

    movies_etl: init app streaming admin

---
+++ services/movies_streaming_admin/entrypoint.dev.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Waiting for postgres..."
+
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  sleep 0.1s
+done
+
+echo "PostgreSQL started"
+
+python manage.py flush --no-input
+python manage.py makemigrations
+python manage.py migrate
+
+echo "from django.contrib.auth.models import User;
+User.objects.filter(email='$DJANGO_ADMIN_EMAIL').delete();
+User.objects.create_superuser('$DJANGO_ADMIN_USERNAME', '$DJANGO_ADMIN_EMAIL', '$DJANGO_ADMIN_PASSWORD');
+" | python manage.py shell
+
+echo "Django superuser $DJANGO_ADMIN_USERNAME created"
+
+python manage.py runserver 0.0.0.0:8000
