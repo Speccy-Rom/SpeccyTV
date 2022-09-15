b42ad0f80b31405cb52d38ea6b0bba2b858ba0be
speccy-rom <speccy.rom@gmail.com>
2022-09-15 22:05:30 +0300

    movies_streaming_admin: add urls settings

--- services/movies_streaming_admin/config/urls.py
+++ services/movies_streaming_admin/config/urls.py
@@ -1,21 +1,32 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from rest_framework import routers
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+from movies.views import FileViewSet, FilmWorkViewSet
+
+schema_view = get_schema_view(
+    openapi.Info(title="Streaming admin API", default_version="v1", description=""),
+    public=True,
+    permission_classes=[]
+)
+
+
+router = routers.DefaultRouter()
+router.register("files", FileViewSet)
+router.register("filmworks", FilmWorkViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('streaming_admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
