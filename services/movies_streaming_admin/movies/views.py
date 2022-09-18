6e40e2142d8170e3c882dc1577fc0f5aba9e04f6
speccy-rom <speccy.rom@gmail.com>
2022-09-18 10:50:10 +0300

    movies_streaming_admin: add views

--- services/movies_streaming_admin/movies/views.py
+++ services/movies_streaming_admin/movies/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.parsers import FileUploadParser, FormParser, MultiPartParser
 
-# Create your views here.
+from movies.models import File, FilmWork
+from movies.serializers import FileSerializer, FilmWorkSerializer
+
+
+class FileViewSet(viewsets.ModelViewSet):
+    parser_classes = [FormParser, MultiPartParser]
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
+    permission_classes = []
+    http_method_names = ['get', 'post', 'put', 'delete']
+
+
+class FilmWorkViewSet(viewsets.ModelViewSet):
+    queryset = FilmWork.objects.all()
+    serializer_class = FilmWorkSerializer
+    permission_classes = []
+    http_method_names = ['get', 'post']
