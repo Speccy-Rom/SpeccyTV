5fd5cd6a6848cf4e66e9a1820dc20d062a9db9d5
speccy-rom <speccy.rom@gmail.com>
2022-09-19 21:03:52 +0300

    movies_streaming_admin: add admin settings

--- services/movies_streaming_admin/movies/admin.py
+++ services/movies_streaming_admin/movies/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import FilmWork, File
+
+
+class FileInLineAdmin(admin.TabularInline):
+    model = FilmWork.files.through
+    extra = 0
+
+
+@admin.register(FilmWork)
+class FilmWorkAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    fields = ('title', 'certificate', 'file_path')
+    inlines = (FileInLineAdmin,)
+    search_fields = ('title', 'files')
+
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'resolution',
+        'fps',
+        'codec_name',
+    )
+    list_filter = (
+        'resolution',
+        'fps',
+        'codec_name',
+    )
+    inlines = (FileInLineAdmin,)
+    search_fields = (
+        'id',
+        'resolution',
+        'fps',
+        'codec_name',
+    )
