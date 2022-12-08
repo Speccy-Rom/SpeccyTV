b6c3d8dca767f0c74b887fa0b1770b259924f401
speccy-rom <speccy.rom@gmail.com>
2022-12-08 23:11:42 +0300

    movies_ugc: init shards

---
+++ services/movies_ugc/benchmarks/mongo/scripts/init_shard_01.js
@@ -0,0 +1,8 @@
+rs.initiate({
+    _id: "mongors1",
+    members: [
+        {_id: 0, host: "mongors1n1"},
+        {_id: 1, host: "mongors1n2"},
+        {_id: 2, host: "mongors1n3"}
+    ]
+})
---
+++ services/movies_ugc/benchmarks/mongo/scripts/init_shard_02.js
@@ -0,0 +1,8 @@
+rs.initiate({
+    _id: "mongors2",
+    members: [
+        {_id: 0, host: "mongors2n1"},
+        {_id: 1, host: "mongors2n2"},
+        {_id: 2, host: "mongors2n3"}
+        ]
+})
