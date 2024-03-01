9e1457cda6381140e0ec7b0abb13b54c0a8fafe7
speccy.root <speccy.rom@gmail.com>
2024-03-01 22:13:48 +0300

    [#240103]: fixed logic main func and added new docs for ElasticsearchLoader

--- services/movies_etl/postgres_to_es/elastic.py
+++ services/movies_etl/postgres_to_es/elastic.py
@@ -7,15 +7,31 @@ import config
 from elasticsearch import Elasticsearch, exceptions
 from utils import backoff
 
+# Logger for this module
 module_logger = logging.getLogger('ElasticsearchLoader')
 
 
 class ElasticsearchLoader:
+    """
+    Class for loading data into Elasticsearch.
+    """
+
     def __init__(self, hosts: list, chunk_size: int = config.ETL_CHUNK_SIZE):
+        """
+        Initialize ElasticsearchLoader with hosts and chunk size.
+
+        :param hosts: List of hosts where Elasticsearch is running.
+        :param chunk_size: Size of the chunk to be loaded at once.
+        """
         self.client = Elasticsearch(hosts=hosts)
         self.chunk_size = chunk_size
 
     def init(self, index_name: str):
+        """
+        Initialize Elasticsearch index.
+
+        :param index_name: Name of the index to be created.
+        """
         index_dir = Path(__file__).resolve(strict=True).parent.joinpath('indexes')
         file = index_dir.joinpath(f'{index_name}.json')
         with open(file, 'r') as index_file:
@@ -26,6 +42,12 @@ class ElasticsearchLoader:
             module_logger.warning('Index already exist: %s', index_name)
 
     def load_to_es(self, records: List[dict], index_name: str) -> None:
+        """
+        Load records into Elasticsearch.
+
+        :param records: List of records to be loaded.
+        :param index_name: Name of the index where records will be loaded.
+        """
         for prepared_query in self._get_chunk_query(records, index_name):
             str_query = '\n'.join(prepared_query) + '\n'
             self._post_to_es(str_query, index_name)
@@ -33,17 +55,36 @@ class ElasticsearchLoader:
 
     @backoff(Exception, logger=module_logger)
     def _post_to_es(self, query: str, index: str) -> None:
+        """
+        Post query to Elasticsearch.
+
+        :param query: Query to be posted.
+        :param index: Name of the index where query will be posted.
+        """
         self.client.bulk(body=query, index=index)
         self.client.indices.refresh(index=index)
 
-    def _get_chunk_query(self, rows: List[dict], index_name: str) -> Generator[List[str], None, None]:
-        chunked_rows = (rows[i:i+self.chunk_size] for i in range(0, len(rows), self.chunk_size))
+    def _get_chunk_query(
+        self, rows: List[dict], index_name: str
+    ) -> Generator[List[str], None, None]:
+        """
+        Generate chunked queries from rows.
+
+        :param rows: List of rows to be chunked.
+        :param index_name: Name of the index where rows will be loaded.
+        :return: Generator of chunked queries.
+        """
+        chunked_rows = (
+            rows[i : i + self.chunk_size] for i in range(0, len(rows), self.chunk_size)
+        )
 
         for chunk in chunked_rows:
             prepared_query = []
             for row in chunk:
-                prepared_query.extend([
-                    json.dumps({'index': {'_index': index_name, '_id': row['id']}}),
-                    json.dumps(row)
-                ])
+                prepared_query.extend(
+                    [
+                        json.dumps({'index': {'_index': index_name, '_id': row['id']}}),
+                        json.dumps(row),
+                    ]
+                )
             yield prepared_query
--- services/movies_etl/postgres_to_es/main.py
+++ services/movies_etl/postgres_to_es/main.py
@@ -7,38 +7,47 @@ from pipelines import FilmWorkPipeline, GenrePipeline, PersonPipeline
 from postgres import PostgresProducer
 from state import JsonFileStorage, State
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format='%(asctime)s: %(name)s - %(levelname)s - %(message)s'
+)
 logger = logging.getLogger('root')
 
 
 def main():
     logger.info('Start ETL application with %s mode', config.ETL_MODE)
 
-    state = State(
-        JsonFileStorage(config.ETL_FILE_STATE)
-    )
+    state = State(JsonFileStorage(config.ETL_FILE_STATE))
     es_loader = ElasticsearchLoader(
-        ['http://{host}:{port}'.format(host=config.ELASTICSEARCH_HOST, port=config.ELASTICSEARCH_PORT)]
+        [
+            'http://{host}:{port}'.format(
+                host=config.ELASTICSEARCH_HOST, port=config.ELASTICSEARCH_PORT
+            )
+        ]
     )
-    db_adapter = PostgresProducer({
-        'dbname': config.POSTGRES_NAME,
-        'user': config.POSTGRES_USER,
-        'password': config.POSTGRES_PASSWORD,
-        'host': config.POSTGRES_HOST,
-        'port': config.POSTGRES_PORT,
-    })
+    db_adapter = PostgresProducer(
+        {
+            'dbname': config.POSTGRES_NAME,
+            'user': config.POSTGRES_USER,
+            'password': config.POSTGRES_PASSWORD,
+            'host': config.POSTGRES_HOST,
+            'port': config.POSTGRES_PORT,
+        }
+    )
 
-    if config.ETL_MODE == ModeETL.FILM_WORK.value:
-        film_work = FilmWorkPipeline(state, db_adapter, es_loader)
-        film_work.etl_process()
-    elif config.ETL_MODE == ModeETL.PERSON.value:
-        person = PersonPipeline(state, db_adapter, es_loader)
-        person.etl_process()
-    elif config.ETL_MODE == ModeETL.GENRE.value:
-        genre = GenrePipeline(state, db_adapter, es_loader)
-        genre.etl_process()
+    pipeline_classes = {
+        ModeETL.FILM_WORK.value: FilmWorkPipeline,
+        ModeETL.PERSON.value: PersonPipeline,
+        ModeETL.GENRE.value: GenrePipeline,
+    }
+
+    if pipeline_class := pipeline_classes.get(config.ETL_MODE):
+        pipeline = pipeline_class(state, db_adapter, es_loader)
+        pipeline.etl_process()
     else:
-        logger.warning('Mode ETL must be from a list: %s', ', '.join([mode.value for mode in ModeETL]))
+        logger.warning(
+            'Mode ETL must be from a list: %s',
+            ', '.join([mode.value for mode in ModeETL]),
+        )
 
     logger.info('End ETL application with %s mode', config.ETL_MODE)
 
