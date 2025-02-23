3fbaac3100505fd931b28a17a4ff414315c6c959
speccy-rom <speccy.rom@gmail.com>
2025-02-23 17:47:22 +0300

    Refactor state management in BaseStorage: enhance type hints and add docstrings

--- services/movies_etl/postgres_to_es/state.py
+++ services/movies_etl/postgres_to_es/state.py
@@ -1,18 +1,30 @@
 import abc
 import json
 import logging
-from typing import Any, Optional
+from typing import Any, Optional, Dict
 
 module_logger = logging.getLogger('JsonFileStorage')
 
 
 class BaseStorage(abc.ABC):
     @abc.abstractmethod
-    def save_state(self, state: dict) -> None:
+    def save_state(self, state: Dict[str, Any]) -> None:
+        """
+        Save the state to the storage.
+
+        Args:
+            state (Dict[str, Any]): The state to save.
+        """
         pass
 
     @abc.abstractmethod
-    def retrieve_state(self) -> dict:
+    def retrieve_state(self) -> Dict[str, Any]:
+        """
+        Retrieve the state from the storage.
+
+        Returns:
+            Dict[str, Any]: The retrieved state.
+        """
         pass
 
 
@@ -20,14 +32,26 @@ class JsonFileStorage(BaseStorage):
     def __init__(self, file_path: Optional[str] = None):
         self.file_path = file_path
 
-    def save_state(self, state: dict) -> None:
+    def save_state(self, state: Dict[str, Any]) -> None:
+        """
+        Save the state to a JSON file.
+
+        Args:
+            state (Dict[str, Any]): The state to save.
+        """
         if self.file_path is None:
-            return None
+            return
 
         with open(self.file_path, 'w') as f:
             json.dump(state, f)
 
-    def retrieve_state(self) -> Optional[dict]:
+    def retrieve_state(self) -> Dict[str, Any]:
+        """
+        Retrieve the state from a JSON file.
+
+        Returns:
+            Dict[str, Any]: The retrieved state.
+        """
         if self.file_path is None:
             module_logger.warning('No state file provided. Continue with in-memory state')
             return {}
@@ -37,19 +61,6 @@ class JsonFileStorage(BaseStorage):
                 data = json.load(f)
             return data
         except FileNotFoundError:
+            module_logger.warning('State file not found. Initializing with empty state.')
             self.save_state({})
-        return {}
-
-
-class State:
-    def __init__(self, storage: BaseStorage):
-        self.storage = storage
-        self.state = storage.retrieve_state() or {}
-
-    def set_state(self, key: str, value: Any) -> None:
-        self.state[key] = value
-        self.storage.save_state(self.state)
-        module_logger.info('Set key %s with value %s to storage', key, value)
-
-    def get_state(self, key: str) -> Any:
-        return self.state.get(key)
+            return {}
